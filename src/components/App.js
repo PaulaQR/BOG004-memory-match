ba35cde2b0431ce549a4325f572477e3e85803e9
PaulaQR <mryquin@gmail.com>
2022-02-10 11:27:27 -0500

    trayendo data

--- src/components/App.js
+++ src/components/App.js
@@ -3,28 +3,37 @@
 // JavasSript que contenga el `export` correspondiente...
 //
  import pokemon from '../data/pokemon/pokemon.js';
-  let data = pokemon;
 
+ let data = pokemon;
+console.log(data);
   const App = () => {
-    const el = document.createElement('div');
-  
-    el.className = 'App';
-    el.textContent = 'Hola mundo!';
-  
-    return el;
-  
+      const informacionTarjeta = aleatorizar();
+      //Generar HTML
+      informacionTarjeta.forEach(() =>{
+    const tarjeta = document.createElement('section');
+    const cara = document.createElement('image');
+    const sello = document.createElement('section');
+    //acceder a la lista de clases de un elemento 
+    tarjeta.classList = "tarjeta";
+    cara.classList = "cara";
+    sello.classList = "sello";
+  //adjuntar cartas en las secciones 
+  section.appendChild(tarjeta); 
+  });
   };
 
   //una funciôn para generar un aleatorio
   // Colocar elementos en distintas posiciones de un array
 
-   const aleatorizar = () => {
-    const informacionTarjeta = data;
+  const aleatorizar = () => {
+    const informacionTarjeta = data();
     informacionTarjeta.sort(() => Math.random() - 0.5);
-    
   };
+
   aleatorizar();
   
+  
+
 
 
 
--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
-import App from "./components/App";
+import App from './components/App.js';
 
  
  const play = document.getElementById('play-button');
 
- play.addEventListener("click", () => {
-
+ play.location("click", () => {
+  
   
 document.getElementById("container").style.display = "none";
 document.getElementById("screen-two").style.display = "block";
 document.getElementById("screen-three").style.display = "block";
 
 
- }  
-
+ })  
 
  
  document.getElementById('root').appendChild(App());
